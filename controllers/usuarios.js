cd1dc537dd1297d20d67ae25f0d1502431ebb9be
Luis Ochoa <occhoa90@gmail.com>
2022-09-13 22:17:10 -0700

    JWT

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -56,15 +56,13 @@ const usuariosPost = async(req, res) => {
 const usuariosDelete = async(req, res) => {
 
     const { id } = req.params;
+    
     // Borrado Fisico
     // const usuario = await Usuario.findByIdAndDelete( id );
 
     const usuario = await Usuario.findByIdAndUpdate( id, { estado: false });
 
-
-    res.json({
-        usuario
-    });
+    res.json(usuario);
 }
 
 const usuariosPatch = (req, res) => {
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { validarCampos } = require('../middlewares/validar-campos');
+
+const { validarCampos, validarJWT, tieneRole } = require('../middlewares');
+
 const { esRoleValido, emailExiste, existeUsuarioPorId } = require('../helpers/db-validators');
 const { usuariosGet, usuariosPatch, usuariosPut, usuariosPost, usuariosDelete } = require('../controllers/usuarios');
 
@@ -28,6 +30,9 @@ router.post('/', [
 ], usuariosPost);
 
 router.delete('/:id',[
+    validarJWT,
+    // esAdminRole,
+    tieneRole('ADMIN_ROLE', 'VENTAS_ROLE'),
     check('id', 'No es un ID válido').isMongoId(),
     check('id').custom( existeUsuarioPorId ),
     validarCampos
